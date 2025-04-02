3f4a7131edaea48df577acc71ac469c2ef7fcfa1
Sid06011993 <siddhantdhagamwar1@gmail.com>
2025-04-02 16:43:02 +0530

    second commit on 2/4/25

--- src/app/lifecyclehooks/lifecyclehooks.component.ts
+++ src/app/lifecyclehooks/lifecyclehooks.component.ts
@@ -6,5 +6,43 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
   styleUrls: ['./lifecyclehooks.component.css']
 })
 export class LifecyclehooksComponent implements OnChanges, OnInit, DoCheck, AfterViewInit, AfterViewChecked, AfterContentChecked, AfterContentInit, OnDestroy{
+  interval:any;
+     
+  ngOnInit():void{
+    console.log('ngOnInit running');
+    this.interval=setInterval(()=>{
+      console.log('count');
+    },5000)
+  }
 
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy running');
+    clearInterval(this.interval);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges running');
+
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck running');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit running');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked running');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit running');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked running');
+  }
 }
